16b4005ab35511e448dd5596b00a72beb00870f8
ilya <ibm.watson@mail.ru>
2024-06-16 20:55:21 +0300

    16-06-2024

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
 from fastapi import FastAPI, Depends, HTTPException, Request, Form, Query
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
@@ -18,13 +18,8 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def read_sites(request: Request, db: Session = Depends(get_db)):
     sites = crud.get_sites(db)
-    return templates.TemplateResponse("index.html", {"request": request, "sites": sites})
-
-
-@app.get("/add-sites", response_class=HTMLResponse)
-async def add_sites(request: Request, db: Session = Depends(get_db)):
     themes = crud.get_themes(db)
-    return templates.TemplateResponse("add_site_custom.html", {"request": request, "themes": themes})
+    return templates.TemplateResponse("dashboard/sites.html", {"request": request, "sites": sites, "themes": themes})
 
 
 @app.get("/sites/{site_id}", response_class=HTMLResponse)
@@ -46,17 +41,18 @@ async def read_post(request: Request, post_id: int, db: Session = Depends(get_db
 @app.get("/themes", response_class=HTMLResponse)
 async def read_themes(request: Request, db: Session = Depends(get_db)):
     themes = crud.get_themes(db)
-    return templates.TemplateResponse("theme_list.html", {"request": request, "themes": themes})
-
-
-@app.get("/add-themes", response_class=HTMLResponse)
-async def add_themes(request: Request, db: Session = Depends(get_db)):
-    return templates.TemplateResponse("add_themes.html", {"request": request})
+    return templates.TemplateResponse("dashboard/theme.html", {"request": request, "themes": themes})
 
 
 @app.post("/sites", response_model=schemas.Site)
-async def create_site(site: schemas.SiteCreate, db: Session = Depends(get_db)):
-    return crud.create_site(db, site)
+async def create_site(domain: str = Form(...),
+                      login: str = Form(...),
+                      password: str = Form(...),
+                      theme_id: int = Form(...),
+                      db: Session = Depends(get_db)):
+    site_create = schemas.SiteCreate(domain=domain, login=login, password=password, theme_id=theme_id)
+    crud.create_site(db, site_create)
+    return RedirectResponse(url="/", status_code=303)
 
 
 @app.post("/posts", response_model=schemas.Post)
@@ -65,8 +61,13 @@ async def create_post(post: schemas.PostCreate, site_id: int = Query(...), db: S
 
 
 @app.post("/themes", response_model=schemas.Theme)
-async def create_theme(theme: schemas.ThemeCreate, db: Session = Depends(get_db)):
-    return crud.create_theme(db, theme)
+async def create_theme(name: str = Form(...),
+                       name_file_to_key: str = Form(...),
+                       folder_to_pict: str = Form(...),
+                       db: Session = Depends(get_db)):
+    theme_create = schemas.ThemeCreate(name=name, name_file_to_key=name_file_to_key, folder_to_pict=folder_to_pict)
+    crud.create_theme(db, theme_create)
+    return RedirectResponse(url='/themes', status_code=303)
 
 
 @app.post("/sites/{site_id}", response_model=schemas.Site)
@@ -86,7 +87,8 @@ async def update_theme(theme_id: int, theme: schemas.ThemeCreate, db: Session = 
 
 @app.post("/sites/{site_id}/delete", response_model=schemas.Site)
 async def delete_site(site_id: int, db: Session = Depends(get_db)):
-    return crud.delete_site(db, site_id)
+    crud.delete_site(db, site_id)
+    return RedirectResponse(url='/', status_code=303)
 
 
 @app.post("/posts/{post_id}/delete", response_model=schemas.Post)
@@ -96,4 +98,5 @@ async def delete_post(post_id: int, db: Session = Depends(get_db)):
 
 @app.post("/themes/{theme_id}/delete", response_model=schemas.Theme)
 async def delete_theme(theme_id: int, db: Session = Depends(get_db)):
-    return crud.delete_theme(db, theme_id)
+    crud.delete_theme(db, theme_id)
+    return RedirectResponse(url='/themes', status_code=303)
